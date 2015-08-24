05da53d9856cb6d07e11b74e479d90db2f1430f5
Sara Simon <smbsimon@gmail.com>
2015-08-24 18:05:47 -0400

    why with the time do i not glance aside

--- sonnet.py
+++ sonnet.py
@@ -1,3 +1,13 @@
+from datetime import timedelta
+
+year = timedelta(days=365)
+seconds = year.total_seconds()
+
+def the_time():
+	for i in range(int(seconds)):
+	    print i
+
+
 my_verse = """
 
     .....
